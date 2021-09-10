cfe62ff01045adc8190178a294e195c3f6f0d72c
Carlos Bruno <cbcarlos08@gmail.com>
2021-09-10 09:24:06 -0400

    categoria

--- api-gateway/src/app.controller.ts
+++ api-gateway/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Logger, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ClientProxy, ClientProxyFactory, Transport } from '@nestjs/microservices'
+import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
-@Controller()
+@Controller('api/v1')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+	private logger = new Logger(AppController.name)
+  	
+	private clientAdminBackend: ClientProxy
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+	constructor() {
+		this.clientAdminBackend = ClientProxyFactory.create({
+			transport: Transport.RMQ,
+			options: {
+				urls: ['amqps://ipdv:ipdvonline2021@b-c33ff9e9-6093-4032-a97c-286274b56c3a.mq.us-east-1.amazonaws.com:5671/smartranking'],
+				queue: 'admin-backend'
+			}
+		})
+	}
+
+	@Post('categoria')
+	@UsePipes(ValidationPipe)
+	async criarCategoria( @Body() criarCategoriaDto: CriarCategoriaDto ){
+		return await this.clientAdminBackend.emit('criar-categoria', criarCategoriaDto)
+	}
+
+	
 }
